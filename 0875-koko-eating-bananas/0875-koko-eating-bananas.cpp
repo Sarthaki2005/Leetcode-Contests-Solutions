7f53c332178c13576f3754cfc3e38f595c242a79
Sarthaki_Bhoir <sarthakibhoir1010@gmail.com>
2026-09-26 00:27:00 +0530

    Time: 7 ms (69.24%), Space: 22.9 MB (89.66%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+using ll=long long;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        int high=*max_element(piles.begin(),piles.end());
+        int low=1;
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            ll th=0;
+             for(int i=0;i<n;i++){
+             th+=( piles[i]+mid-1)/mid;
+             }
+             if(th<=h){
+                ans=mid;
+                high=mid-1;
+             }else{
+                low=mid+1;
+             }
+        }
+        return ans;
+    }
+};
